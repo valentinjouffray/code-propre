a1f0099b8aee28a5cb052c45bd496ba6a423b046
valentin <valentin.jouffray@gmail.com>
2023-03-27 15:35:18 +0200

    feat(ex1): ex1 terminé

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,24 @@
+package ex1;
+
+public class Calcul {
+
+	/**
+	 * <p>Donne la somme de l'addition de a par b</p>
+	 * @param a première valeur
+	 * @param b deuxième valeur
+	 * @return	Somme de a par b
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+
+	/**
+	 * <p>Donne la somme de la soustraction de a par b</p>
+	 * @param a première valeur
+	 * @param b deuxième valeur
+	 * @return	Différence de a par b
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,21 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+	public int siret;
+	public String nom;
+	public String adresse;
+	public Date dateCreation;
+	
+	public static final int capitalMax = 3000000;
+
+	/**
+	 * Affiche le statut de la Personne
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
